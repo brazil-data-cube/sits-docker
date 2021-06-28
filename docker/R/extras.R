3bd8d1a187958c48c1c040b2f83da09ca1129219
Phelps <phelpsmemo@gmail.com>
2021-06-28 16:07:27 -0300

    Update sits package to v0.12.0 (Close #10, Close #12, Close #13)

--- docker/R/extras.R
+++ docker/R/extras.R
@@ -1,5 +1,6 @@
 # Install devtools, rmarkdown, knitr, testthat and Rcpp if not already available
-install.packages(c("rmarkdown", "Rcpp", "knitr", "testthat", "remotes", "qpdf"))
+install.packages(c("rmarkdown", "Rcpp", "knitr", 
+                   "testthat", "remotes", "qpdf", "shiny"))
 
 # check the environment type
 environment_type <- Sys.getenv("SITS_ENVIRONMENT_TYPE")
@@ -8,5 +9,5 @@ environment_type <- if (environment_type == "full") TRUE else NA
 remotes::install_deps(dependencies = environment_type)
 
 # install keras
-remotes::install_github("rstudio/reticulate@7174f745626d3e71a9527a96d56075a729b6506e")
-remotes::install_github("rstudio/keras@aaddf0e411f66d48f88f858d1436c1d38e13fb97")
+remotes::install_github("rstudio/reticulate@1.19")
+remotes::install_github("rstudio/keras@v2.4.0")
--- docker/sits/install_sits.R
+++ docker/sits/install_sits.R
@@ -1,3 +1,3 @@
-remotes::install_github('e-sensing/sits@v0.9.8', dependencies = FALSE)
-remotes::install_github("e-sensing/wtss@v2.2.0", dependencies = FALSE)
-remotes::install_github("e-sensing/inSitu@v1.2", dependencies = FALSE)
+remotes::install_github('e-sensing/sits@v0.12.0', dependencies = FALSE)
+remotes::install_github("e-sensing/wtss@v0.9.1", dependencies = FALSE)
+remotes::install_github("e-sensing/sitsdata@7b3d4fe885c84cad0511bf7141c89f0c169d8f21", dependencies = FALSE)
