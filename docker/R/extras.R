3438c8218b4e823475773e44586666aa1d46c9d5
Phelps <phelpsmemo@gmail.com>
2021-06-29 23:27:51 -0300

    fix Rwtss and update comments

--- docker/R/extras.R
+++ docker/R/extras.R
@@ -1,13 +1,25 @@
+#
 # Install devtools, rmarkdown, knitr, testthat and Rcpp if not already available
+#
 install.packages(c("rmarkdown", "Rcpp", "knitr", 
-                   "testthat", "remotes", "qpdf", "shiny"))
+                   "testthat", "remotes", "qpdf", 
+                   "shiny",   "pacman"))
 
-# check the environment type
+#
+# Check the environment type
+#
 environment_type <- Sys.getenv("SITS_ENVIRONMENT_TYPE")
 environment_type <- if (environment_type == "full") TRUE else NA
 
 remotes::install_deps(dependencies = environment_type)
 
-# install keras
+#
+# Install keras
+#
 remotes::install_github("rstudio/reticulate@1.19")
 remotes::install_github("rstudio/keras@v2.4.0")
+
+#
+# Web Time Series Service Client
+#
+install.packages("Rwtss")
--- docker/sits/install_sits.R
+++ docker/sits/install_sits.R
@@ -1,3 +1,9 @@
-remotes::install_github('e-sensing/sits@v0.12.0', dependencies = FALSE)
-remotes::install_github("e-sensing/wtss@v0.9.1", dependencies = FALSE)
+#
+# Satellite Image Time Series
+#
+remotes::install_github("e-sensing/sits@v0.12.0", dependencies = FALSE)
+
+#
+# Satellite Image Time Series Example Datasets
+#
 remotes::install_github("e-sensing/sitsdata@7b3d4fe885c84cad0511bf7141c89f0c169d8f21", dependencies = FALSE)
