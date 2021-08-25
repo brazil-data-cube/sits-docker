18abf5be8051e2d07082eef64e6b78f059f46be5
Phelps <phelpsmemo@gmail.com>
2021-08-25 15:25:09 -0300

    update to sits v0.13.0 (Closes #15)

--- docker/sits/install_sits.R
+++ docker/sits/install_sits.R
@@ -1,9 +1,9 @@
 #
 # Satellite Image Time Series
 #
-remotes::install_github("e-sensing/sits@v0.12.0", dependencies = FALSE)
+remotes::install_github("e-sensing/sits@v0.13.0-3", dependencies = FALSE)
 
 #
 # Satellite Image Time Series Example Datasets
 #
-remotes::install_github("e-sensing/sitsdata@7b3d4fe885c84cad0511bf7141c89f0c169d8f21", dependencies = FALSE)
+remotes::install_github("e-sensing/sitsdata@39449b3d5b77b30f5cdb4cc99d23308082fd9fd4", dependencies = FALSE)
