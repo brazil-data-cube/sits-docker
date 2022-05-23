ac1ad493ca2550cd50ec039b52828ae446ddcfd2
Felipe <lipecaso@gmail.com>
2022-05-22 22:56:02 -0300

    update sits docker 0.17.0

--- docker/sits/install.R
+++ docker/sits/install.R
@@ -12,6 +12,11 @@ sits_tag_version <- Sys.getenv("SITS_TAG_VERSION")
 remotes::install_github(paste0("e-sensing/sits@", sits_tag_version), dependencies = FALSE)
 
 #
+# Change the download method 
+#
+options(download.file.method = "wget") 
+
+#
 # Satellite Image Time Series Example Datasets
 #
 sitsdata_commit_ref <- Sys.getenv("SITSDATA_COMMIT_REF")
