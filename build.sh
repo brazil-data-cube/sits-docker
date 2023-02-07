6a267be6a2938bd82eed5767523459654c1cf577
gqueiroz <gribeiro.mg@gmail.com>
2023-02-07 08:36:02 -0300

    Fix commented lines

--- build.sh
+++ build.sh
@@ -70,10 +70,10 @@ cd base
 SITS_BASE_IMAGE="jupyter/base-notebook:ubuntu-${SITS_UBUNTU_VERSION}"
 SITS_BASE_IMAGE_TAG="${SITS_TAG_PREFIX}/sits-base:${SITS_TAG_VERSION}"
 
-#docker build ${SITS_BUILD_MODE} \
-#       --build-arg BASE_IMAGE=${SITS_BASE_IMAGE} \
-#       -t ${SITS_BASE_IMAGE_TAG} \
-#       --file Dockerfile .
+docker build ${SITS_BUILD_MODE} \
+       --build-arg BASE_IMAGE=${SITS_BASE_IMAGE} \
+       -t ${SITS_BASE_IMAGE_TAG} \
+       --file Dockerfile .
 
 #
 # Build R image with all the package dependencies already installed
@@ -82,12 +82,12 @@ echo "Building base R image for SITS..."
 cd ../r
 
 SITS_R_DOCKER_IMAGE_TAG="${SITS_TAG_PREFIX}/sits-r:${SITS_TAG_VERSION}"
-#docker build ${SITS_BUILD_MODE} \
-#       --build-arg BASE_IMAGE=${SITS_BASE_IMAGE_TAG} \
-#       --build-arg SITS_TAG_VERSION=v${SITS_TAG_VERSION} \
-#       --build-arg SITS_ENVIRONMENT_TYPE=${SITS_ENVIRONMENT_TYPE} \
-#       -t ${SITS_R_DOCKER_IMAGE_TAG} \
-#       --file Dockerfile  .
+docker build ${SITS_BUILD_MODE} \
+       --build-arg BASE_IMAGE=${SITS_BASE_IMAGE_TAG} \
+       --build-arg SITS_TAG_VERSION=v${SITS_TAG_VERSION} \
+       --build-arg SITS_ENVIRONMENT_TYPE=${SITS_ENVIRONMENT_TYPE} \
+       -t ${SITS_R_DOCKER_IMAGE_TAG} \
+       --file Dockerfile  .
 
 #
 # Build final SITS image
@@ -96,12 +96,12 @@ echo "Building SITS image..."
 cd ../sits
 
 SITS_DOCKER_IMAGE_TAG="${SITS_TAG_PREFIX}/sits:${SITS_TAG_VERSION}"
-#docker build ${SITS_BUILD_MODE} \
-#       --build-arg BASE_IMAGE=${SITS_R_DOCKER_IMAGE_TAG} \
-#       --build-arg SITS_TAG_VERSION=v${SITS_TAG_VERSION} \
-#       --build-arg SITSDATA_COMMIT_REF=${SITSDATA_COMMIT_REF} \
-#       -t ${SITS_DOCKER_IMAGE_TAG} \
-#       --file Dockerfile  .
+docker build ${SITS_BUILD_MODE} \
+       --build-arg BASE_IMAGE=${SITS_R_DOCKER_IMAGE_TAG} \
+       --build-arg SITS_TAG_VERSION=v${SITS_TAG_VERSION} \
+       --build-arg SITSDATA_COMMIT_REF=${SITSDATA_COMMIT_REF} \
+       -t ${SITS_DOCKER_IMAGE_TAG} \
+       --file Dockerfile  .
 
 #
 # Build RStudio for SITS image
@@ -110,10 +110,10 @@ echo "Building RStudio for SITS image..."
 cd ../rstudio
 
 SITS_RSTUDIO_DOCKER_IMAGE_TAG="${SITS_TAG_PREFIX}/sits-rstudio:${SITS_TAG_VERSION}"
-#docker build ${SITS_BUILD_MODE} \
-#       --build-arg BASE_IMAGE=${SITS_DOCKER_IMAGE_TAG} \
-#       -t ${SITS_RSTUDIO_DOCKER_IMAGE_TAG} \
-#       --file Dockerfile  .
+docker build ${SITS_BUILD_MODE} \
+       --build-arg BASE_IMAGE=${SITS_DOCKER_IMAGE_TAG} \
+       -t ${SITS_RSTUDIO_DOCKER_IMAGE_TAG} \
+       --file Dockerfile  .
 
 #
 # Build Jupyter for SITS image
